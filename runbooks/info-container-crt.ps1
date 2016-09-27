2e85512d15afabaad682383a3142c60288d7cd1c
Ashwin Sebastian <asebastian@sysgain.com>
2016-09-27 23:06:53 +0530

    up

--- runbooks/info-container-crt.ps1
+++ runbooks/info-container-crt.ps1
@@ -1,9 +1,7 @@
 workflow container1 {
     param(
      
-        [Parameter(Mandatory=$true)]
-        [string]
-        $credentialName,
+       
 
         [Parameter(Mandatory=$true)]
         [string]
@@ -16,18 +14,13 @@ workflow container1 {
 
     InlineScript{
 
-    $credentialName= $Using:credentialName  
+   
     $adfStorageAccName = $Using:adfStorageAccName
 	$adfStorageAccKey = $Using:adfStorageAccKey
-    Write-Output $credentialName,
+
     Write-Output $adfStorageAccName,
     Write-Output $adfStorageAccKey
 
-
-    $Cred = Get-AutomationPSCredential -Name $credentialName
-
-    Add-AzureRmAccount -Credential $Cred
-    
     $storageCtx = New-AzureStorageContext -StorageAccountName $adfStorageAccName -StorageAccountKey $adfStorageAccKey
 	
     New-AzureStorageContainer -Name "adfgetstarted" -Context $storageCtx
